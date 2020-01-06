fafab006e8b073ddf5722bf83dec8f58f0203a08
Tyler Casperson <tylercasperson@gmail.com>
2020-01-06 15:03:21 -0700

    added the ability to pick a random word

---
+++ javascript/game.js
@@ -0,0 +1,31 @@
+
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','-','ä','ö','ü'];
+var geusses = [];
+var correctWord = [];
+
+var defaultWords = ['one', 'two', 'three'];
+var wordBox = [];
+var gameWords;
+
+if (wordBox.length = 0) 
+{
+    gameWords = defaultWords;
+} else {
+    gameWords = wordBox;
+};
+
+var gameWords = ['one', 'two', 'three'];
+randomPick = Math.floor(Math.random()*defaultWords.length);
+correctWord = defaultWords[randomPick];
+
+
+
+
+
+
+correctWord = 
+
+
+document.onkeyup = function(event) {
+//write a function that passes the different variables checking the status
+};
